db7ed95c634c61e1af326daf7b9c3b1d2b363f97
osdoonhyun <skw1996@naver.com>
2023-07-18 18:04:23 +0900

    feat: 질문 데이터 API 연결

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Question from '../components/Question';
 import RandomQuestion from '../components/RandomQuestion';
 
 export default function Home() {
+  const [questions, setQuestions] = useState([]);
+
+  const getQuestions = async () => {
+    try {
+      const { data, status } = await axios.get(
+        'http://localhost:1337/api/questions'
+      );
+
+      if (status === 200) {
+        setQuestions(data.data);
+      }
+    } catch (error) {
+      console.log('Get Question Error', error.message);
+    }
+  };
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
   return (
     <>
       {/* Home 입니다. */}
