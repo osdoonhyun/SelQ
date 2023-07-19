041f3b4a5e4b8b616c25f3c75e957d23a0b58b67
osdoonhyun <skw1996@naver.com>
2023-07-19 21:00:49 +0900

    feat: 질문 답변 UI 추가

---
+++ src/components/Answer.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Button, Collapse } from 'react-bootstrap';
+
+export default function Answer({ description }) {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(false);
+  }, [description]);
+
+  return (
+    <div style={{ display: 'inline-block', marginTop: '1.2rem' }}>
+      <Button
+        variant='outline-dark'
+        onClick={() => setOpen(!open)}
+        aria-controls='example-collapse-text'
+        aria-expanded={open}
+      >
+        A.
+      </Button>
+      <Collapse in={open}>
+        <div
+          id='example-collapse-text'
+          style={{
+            marginTop: '0.8rem',
+            fontSize: '1.3rem',
+            fontWeight: '400',
+            lineHeight: 1.2,
+            letterSpacing: '0.1rem',
+          }}
+        >
+          {description}
+        </div>
+      </Collapse>
+    </div>
+  );
+}
