63542bc23f3a2238f0943e90cd34bdd436e4af49
osdoonhyun <skw1996@naver.com>
2023-07-20 19:45:17 +0900

    fix: 첫 렌더링시 질문 데이터 없는 버그 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import RandomQuestion from '../components/RandomQuestion';
 
 export default function Home() {
   const [questions, setQuestions] = useState([]);
+  const [nextQuestion, setNextQuestion] = useState({});
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
   const getQuestions = async () => {
     try {
@@ -14,10 +16,35 @@ export default function Home() {
 
       if (status === 200) {
         setQuestions(data.data);
+        setNextQuestion(getRandomQuestion(data.data, data.data.length));
       }
     } catch (error) {
       console.log('Get Question Error', error.message);
     }
+  };
+
+  const getNextQuestion = (category) => {
+    let randomQuestions = [...questions];
+    if (category !== 'All') {
+      randomQuestions = questions.filter(
+        (question) => question.attributes.category === category
+      );
+    }
+
+    const numberOfQuestions = randomQuestions.length;
+    const randomQuestion = getRandomQuestion(questions, numberOfQuestions);
+
+    setNextQuestion(randomQuestion);
+  };
+
+  const getRandomQuestion = (questions, count) => {
+    const randomIndex = Math.floor(Math.random() * count);
+    return questions[randomIndex];
+  };
+
+  const handleDropdownCategorySelect = (eventKey) => {
+    console.log('이벤트클릭', eventKey);
+    setSelectedCategory(eventKey);
   };
 
   useEffect(() => {
@@ -26,10 +53,12 @@ export default function Home() {
 
   return (
     <>
-      {/* Home 입니다. */}
-      {/* {data} */}
-      <RandomQuestion />
-      <Question />
+      <RandomQuestion
+        getNextQuestion={getNextQuestion}
+        selectedCategory={selectedCategory}
+        onSelect={handleDropdownCategorySelect}
+      />
+      <Question question={nextQuestion} />
     </>
   );
 }
