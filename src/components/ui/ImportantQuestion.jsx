b0331d96805deed5ea2b0a8d6c22b34374dcb39f
osdoonhyun <skw1996@naver.com>
2023-07-28 21:33:29 +0900

    feat: 중요도 표시한 질문 모아보기 페이지 API 연결,  UI 구현

--- src/components/ui/ImportantQuestion.jsx
+++ src/components/ui/ImportantQuestion.jsx
@@ -1,35 +1,60 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Toast, ToastContainer } from 'react-bootstrap';
 
-export default function ImportantQuestion({ questionId }) {
+const IMPORTANCE_OPTIONS = [
+  { label: '안중요', level: 0, color: 'white', var: 'light' },
+  { label: '중요', level: 1, color: 'yellow', var: 'success' },
+  { label: '매우중요', level: 2, color: 'orange', var: 'warning' },
+  { label: '매우매우중요', level: 3, color: 'red', var: 'danger' },
+];
+
+export default function ImportantQuestion({ importance, importances }) {
   const [added, setAdded] = useState(false);
   const [show, setShow] = useState(false);
-
+  const [importanceLevel, setImportanceLevel] = useState(importance);
+  console.log('중요도s,', importances); // 이렇게 하면 undefined 값이 나옴
+  console.log('중요도,', importance);
   const handleAddClick = () => {
-    addImportant(questionId);
+    // addImportant(questionId);
+    increaseImportance();
+    console.log('중요도~~', importanceLevel);
   };
 
-  const addImportant = async (questionId) => {
-    try {
-      const userInput = {
-        data: {
-          question: questionId,
-          memo: '',
-        },
-      };
-      const { status } = await axios.post(
-        'http://localhost:1337/api/importants',
-        userInput
-      );
-      if (status === 200) {
-        setAdded((prev) => !prev);
-        setShow((prev) => !prev);
+  // const addImportant = async (questionId) => {
+  //   try {
+  //     const userInput = {
+  //       data: {
+  //         question: questionId,
+  //         memo: '',
+  //       },
+  //     };
+  //     const { status } = await axios.post(
+  //       'http://localhost:1337/api/importants',
+  //       userInput
+  //     );
+  //     if (status === 200) {
+  //       setAdded((prev) => !prev);
+  //       setShow((prev) => !prev);
+  //     }
+  //   } catch (error) {
+  //     console.log('Add Important', error.message);
+  //   }
+  // };
+
+  const increaseImportance = () => {
+    setImportanceLevel((prevLevel) => {
+      if (prevLevel + 1 > 3) {
+        return 0;
       }
-    } catch (error) {
-      console.log('Add Important', error.message);
-    }
+      return prevLevel + 1;
+    });
   };
+
+  useEffect(() => {
+    setImportanceLevel(importance);
+  }, [importance]);
+
   return (
     <>
       <ToastContainer
@@ -40,7 +65,7 @@ export default function ImportantQuestion({ questionId }) {
         <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide>
           <Toast.Header>
             <strong className='me-auto'>
-              {added ? '북마크 추가' : '북마크 해제'}
+              {/* {added ? '북마크 추가' : '북마크 해제'} */}별
             </strong>
           </Toast.Header>
           <Toast.Body className='m-auto'>
@@ -49,8 +74,12 @@ export default function ImportantQuestion({ questionId }) {
         </Toast>
       </ToastContainer>
 
-      <Button variant={added ? 'dark' : 'light'} onClick={handleAddClick}>
-        북마크
+      {/* <Button variant={added ? 'dark' : 'light'} onClick={handleAddClick}> */}
+      <Button
+        variant={IMPORTANCE_OPTIONS[importanceLevel || 0]?.var}
+        onClick={handleAddClick}
+      >
+        북마크: {importanceLevel}
       </Button>
     </>
   );
