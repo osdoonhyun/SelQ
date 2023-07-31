df949076ed8daad8a9da230a19d3885e17dfa573
osdoonhyun <skw1996@naver.com>
2023-07-31 20:59:03 +0900

    style: 배민 을지로체 폰트 적용

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,22 @@ import FormContainer from './FormContainer';
 export default function Footer() {
   return (
     <FormContainer>
-      <>신승현입니다.</>
+      <div
+        style={{
+          height: '60px',
+          position: 'absolute',
+          transform: 'translateY(-100%)',
+          borderTop: '1px solid #333',
+          left: 0,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontFamily: 'BMEuljiro10yearslater',
+        }}
+      >
+        @ 2023. shinseunghyun all rights reserved.
+      </div>
     </FormContainer>
   );
 }
