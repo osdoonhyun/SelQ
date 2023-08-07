e755f44ecc3b8c793c2a577134032cd3a610a88d
osdoonhyun <skw1996@naver.com>
2023-08-07 22:06:18 +0900

    refactor: styled-components 적용, Navbar에 active 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,44 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import FormContainer from './FormContainer';
 import { FontSizeSettings } from './ui/FontSizeSettings';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faList } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark } from '@fortawesome/free-regular-svg-icons';
+import { HeaderIcon, HeaderNav } from '../styles/ButtonStyles';
+import { useLocation } from 'react-router-dom';
+
+const NavItem = ({ href, icon, text }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const isActive = currentPath === href;
+
+  return (
+    <Nav.Link href={href}>
+      <HeaderIcon icon={icon} size='xl' isActive={isActive} />
+      <span
+        style={{ fontSize: '13px', color: isActive ? '#5BACEE' : '#B3B3B5' }}
+      >
+        {text}
+      </span>
+    </Nav.Link>
+  );
+};
 
 export default function Header() {
   return (
     <FormContainer>
-      <Navbar className='bg-body-tertiary'>
+      <Navbar style={{ backgroundColor: '#F7F6F7' }} className='bg-body-light'>
         <Container>
           <Navbar.Brand
             href='/'
-            style={{ fontSize: '26px', fontWeight: '600' }}
+            style={{ fontSize: '26px', fontWeight: '600', color: '#5bacee' }}
           >
             Sel-Q
           </Navbar.Brand>
-          <Nav className='me-auto'>
-            <Nav.Link
-              href='/home'
-              className='d-flex flex-column align-items-center'
-            >
-              <FontAwesomeIcon icon={faHouse} size='xl' />
-              <span style={{ fontSize: '13px', marginTop: '8px' }}>홈</span>
-            </Nav.Link>
-            <Nav.Link
-              href='/questions'
-              className='d-flex flex-column align-items-center'
-            >
-              <FontAwesomeIcon icon={faList} size='xl' />
-              <span style={{ fontSize: '13px', marginTop: '8px' }}>
-                질문목록
-              </span>
-            </Nav.Link>
-            <Nav.Link
-              href='/importants'
-              className='d-flex flex-column align-items-center'
-            >
-              <FontAwesomeIcon icon={faBookmark} size='xl' />
-              <span style={{ fontSize: '13px', marginTop: '8px' }}>
-                중요질문
-              </span>
-            </Nav.Link>
-          </Nav>
+          <HeaderNav className='me-auto'>
+            <NavItem href='/home' icon={faHouse} text='홈' />
+            <NavItem href='/questions' icon={faList} text='질문목록' />
+            <NavItem href='/importants' icon={faBookmark} text='중요질문' />
+          </HeaderNav>
           <Nav className='ml-auto'>
             <FontSizeSettings />
           </Nav>
