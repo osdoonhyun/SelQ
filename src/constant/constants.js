4694d3f32ee5caa4d2f62aa241c716618450f89d
osdoonhyun <skw1996@naver.com>
2023-08-09 23:47:46 +0900

    feat: 상수 및 옵션 데이터 추가

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,1 +1,21 @@
-export const CATEGORIES = ['All', 'JavaScript', 'TypeScript', 'React'];
+export const CATEGORIES = [
+  'All',
+  'HTML/CSS',
+  'JavaScript',
+  'React',
+  'TypeScript',
+  'CS',
+];
+
+export const FONT_SIZE_OPTIONS = [
+  { label: '축소', size: '15px', variant: 'small' },
+  { label: '기본', size: '18px', variant: 'basic' },
+  { label: '확대', size: '21px', variant: 'large' },
+];
+
+export const IMPORTANCE_OPTIONS = [
+  { label: '안중요', level: 0, color: 'grey' },
+  { label: '중요', level: 1, color: '#F2D035' },
+  { label: '매우중요', level: 2, color: '#F2D035' },
+  { label: '매우매우중요', level: 3, color: '#F2D035' },
+];
