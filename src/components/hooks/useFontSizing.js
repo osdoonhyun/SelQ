0bfdc2c33d9500dd87522b8d31e90acc46e5a2a8
osdoonhyun <skw1996@naver.com>
2023-08-28 23:47:36 +0900

    refactor: calcFontSize 함수 추출, useFontSizing 훅 리팩토링

--- src/components/hooks/useFontSizing.js
+++ src/components/hooks/useFontSizing.js
@@ -1,36 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { convertUnitToPx } from '../utils/utils';
 import { FONT_SIZE_OPTIONS } from '../../constant/constants';
+
+const calcFontSize = (fontSize, fontVariant) => {
+  const fontSizeValue = convertUnitToPx(fontSize);
+  let calcedFontSize;
+  switch (fontVariant) {
+    case 'small':
+      calcedFontSize = `${fontSizeValue - 3}px`;
+      break;
+    case 'basic':
+      calcedFontSize = fontSize;
+      break;
+    case 'large':
+      calcedFontSize = `${fontSizeValue + 3}px`;
+      break;
+    default:
+      calcedFontSize = fontSize;
+      break;
+  }
+
+  return calcedFontSize;
+};
 
 export default function useFontSizing() {
   const [fontSizing, setFontSizing] = useState('basic');
 
-  const handleFontSizing = (selectedIndex) => {
-    const updatedFontSize = FONT_SIZE_OPTIONS[selectedIndex].variant;
-    setFontSizing(updatedFontSize);
-    localStorage.setItem('fontSizeOption', updatedFontSize);
-  };
-
-  const calcFontSize = (fontSize, fontVariant) => {
-    const fontSizeValue = convertUnitToPx(fontSize);
-    let calcedFontSize;
-    switch (fontVariant) {
-      case 'small':
-        calcedFontSize = `${fontSizeValue - 3}px`;
-        break;
-      case 'basic':
-        calcedFontSize = fontSize;
-        break;
-      case 'large':
-        calcedFontSize = `${fontSizeValue + 3}px`;
-        break;
-      default:
-        calcedFontSize = fontSize;
-        break;
-    }
-
-    return calcedFontSize;
-  };
+  const handleFontSizing = useCallback(
+    (selectedIndex) => {
+      const updatedFontSize = FONT_SIZE_OPTIONS[selectedIndex]?.variant;
+      setFontSizing(updatedFontSize);
+      localStorage.setItem('fontSizeOption', updatedFontSize);
+    },
+    [setFontSizing]
+  );
 
   useEffect(() => {
     const savedFontSizeOption = localStorage.getItem('fontSizeOption');
