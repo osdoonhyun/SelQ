fc9b473d3d40b1b97c1a0ef34a4980795c52a94f
osdoonhyun <skw1996@naver.com>
2023-09-14 23:52:50 +0900

    chore: react-cookie 설치, 적용

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import LayoutContainer from './components/common/LayoutContainer';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { CookiesProvider } from 'react-cookie';
 
 const client = new QueryClient();
 
@@ -15,7 +18,11 @@ function App() {
     <LayoutContainer>
       <FontSizingProvider>
         <QueryClientProvider client={client}>
-          <RouterProvider router={router} />
+          <CookiesProvider>
+            <Provider store={store}>
+              <RouterProvider router={router} />
+            </Provider>
+          </CookiesProvider>
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </FontSizingProvider>
