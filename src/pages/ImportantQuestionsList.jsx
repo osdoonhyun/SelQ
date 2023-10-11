ce7dc354a0f712d5dcd63d643e1f4412e654ccd6
osdoonhyun <skw1996@naver.com>
2023-10-11 23:15:39 +0900

    feat: 중요도 페이지 필터링 옵션 수정

--- src/pages/ImportantQuestionsList.jsx
+++ src/pages/ImportantQuestionsList.jsx
@@ -2,44 +2,172 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import CustomBadge from '../components/ui/CustomBadge';
 import ImportanceCount from '../components/ImportanceCount';
-import { Col, Dropdown, DropdownButton, Row } from 'react-bootstrap';
+import {
+  Badge,
+  ButtonGroup,
+  CloseButton,
+  Col,
+  Dropdown,
+  DropdownButton,
+  Form,
+  Row,
+  Stack,
+} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useFontSize } from '../components/context/FontSizingProvider';
 import { QuestionQ, QuestionTitle } from '../styles/Styles';
-import { useImportantQuestionsQuery } from '../services/api';
+import { useFilteredQuestionQuery } from '../services/api';
 import Pagination from '../components/common/Pagination';
-import { ImportantQuestionsFilterOptions } from '../constant/constants';
+import { IMPORTANCE_FILTER_OPTION } from '../constant/constants';
 
 export default function ImportantQuestionsList() {
   const { fontSizing, calcFontSize } = useFontSize();
   const [currentPage, setCurrentPage] = useState(1);
-  const [selectedFilterOption, setSelectedFilterOption] = useState('');
-
-  const { data: importantQuestions } = useImportantQuestionsQuery(
-    currentPage,
-    selectedFilterOption
+  const [selectedFilterOption, setSelectedFilterOption] = useState({});
+  const [importanceOption, setImportanceOption] = useState(
+    IMPORTANCE_FILTER_OPTION
   );
 
-  const handleFilterOptionClick = (eventKey) => {
-    setSelectedFilterOption(eventKey);
+  const {
+    data: importantQuestions,
+    loading,
+    error,
+  } = useFilteredQuestionQuery(currentPage, selectedFilterOption);
+
+  const handleImportanceClick = (label, options) => {
+    const updatedOptions = options
+      .find((option) => option.label === label)
+      .options.filter((option) => option.isChecked);
+    setSelectedFilterOption({
+      ...selectedFilterOption,
+      [label]: updatedOptions,
+    });
+  };
+
+  const handleImportanceOptionCheck = (e, label) => {
+    const { value, checked } = e.target;
+    const updatedOptions = importanceOption?.map((optionGroup) => {
+      if (optionGroup.label !== label) {
+        return optionGroup;
+      }
+
+      const updatedOptions = optionGroup.options.map((option) => {
+        if (value === '전체선택') {
+          return { ...option, isChecked: checked };
+        } else if (option.value === value) {
+          return { ...option, isChecked: checked };
+        }
+        return option;
+      });
+      return { ...optionGroup, options: updatedOptions };
+    });
+    setImportanceOption(updatedOptions);
+  };
+
+  const handleDeleteOption = (label, optionValue, optionIndex) => {
+    const updatedOptions = { ...selectedFilterOption };
+    updatedOptions[label] = updatedOptions[label].filter(
+      (_, index) => index !== optionIndex
+    );
+    setSelectedFilterOption((prevOptions) => ({
+      ...prevOptions,
+      [label]: updatedOptions[label],
+    }));
+
+    if (label === '중요도') {
+      const updatedImportanceOptions = [...importanceOption];
+      updatedImportanceOptions[0].options =
+        updatedImportanceOptions[0].options.map((option) => {
+          if (option.value === optionValue) {
+            return {
+              ...option,
+              isChecked: false,
+            };
+          }
+          return option;
+        });
+      setImportanceOption(updatedImportanceOptions);
+    }
   };
 
   return (
     <>
-      <div className='d-flex justify-content-end'>
+      {importanceOption?.map((filterOption) => (
         <DropdownButton
+          className='p-1'
+          onToggle={(isOpen) => {
+            if (!isOpen) {
+              handleImportanceClick(filterOption.label, importanceOption);
+            }
+          }}
           variant='Light'
-          id='dropdown-item-button'
-          onSelect={handleFilterOptionClick}
-          title='중요도'
+          as={ButtonGroup}
+          key={filterOption.label}
+          id={`dropdown-variants-${filterOption.label}`}
+          title={filterOption.label}
         >
-          {ImportantQuestionsFilterOptions?.map((filterOption, index) => (
-            <Dropdown.Item key={index} eventKey={filterOption.label}>
-              {filterOption.label}
-            </Dropdown.Item>
-          ))}
+          <>
+            <Stack style={{ padding: '0 16px' }} direction='horizontal'>
+              <Form.Check
+                type='checkbox'
+                value='전체선택'
+                tag={filterOption.label}
+                onChange={(e) =>
+                  handleImportanceOptionCheck(e, filterOption.label)
+                }
+                checked={importanceOption
+                  .find(
+                    (optionGroup) => optionGroup.label === filterOption.label
+                  )
+                  ?.options.every((option) => option.isChecked)}
+              />
+              <span className='ms-auto'>전체선택</span>
+            </Stack>
+            <hr />
+            {filterOption.options.map((option, index) => (
+              <Stack
+                key={index}
+                style={{ padding: '0 16px' }}
+                direction='horizontal'
+              >
+                <Form.Check
+                  type='checkbox'
+                  onChange={(e) =>
+                    handleImportanceOptionCheck(e, filterOption.label)
+                  }
+                  checked={option.isChecked}
+                  value={option.value}
+                />
+                <span className='ms-auto'>{option.value}</span>
+              </Stack>
+            ))}
+          </>
         </DropdownButton>
-      </div>
+      ))}
+      <Stack direction='horizontal' gap={2} className='p-2'>
+        {Object.keys(selectedFilterOption).map((label, index) =>
+          selectedFilterOption[label].map((option, optionIndex) => (
+            <Badge
+              bg='#5bacee'
+              style={{
+                fontSize: '0.8rem',
+                color: '#fff',
+                letterSpacing: '0.1rem',
+                backgroundColor: '#5bacee',
+              }}
+              key={optionIndex}
+              className='d-flex justify-content-center align-items-center '
+            >
+              <span style={{ marginRight: '5px' }}>{option.value}</span>
+              <CloseButton
+                onClick={() => {
+                  handleDeleteOption(label, option.value, optionIndex);
+                }}
+              />
+            </Badge>
+          ))
+        )}
+      </Stack>
 
       {importantQuestions?.data?.map((question, index) => (
         <LinkContainer to={`/importants/${question.id}`} key={question.id}>
