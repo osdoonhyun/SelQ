a7202032e9989fa2c3496e960711788dfd145e8b
osdoonhyun <skw1996@naver.com>
2023-11-14 16:37:55 +0900

    fix: 환경 변수 수정

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const serverApi = axios.create({
-  // baseURL: process.env.PROD_BASE_URL,
-  baseURL: process.env.DEV_BASE_URL,
+  // baseURL: process.env.REACT_APP_PROD_BASE_URL,
+  baseURL: process.env.REACT_APP_DEV_BASE_URL,
   withCredentials: true,
 });
