7aab758fcba23e99e192306ce26152e5e1f38d8c
osdoonhyun <skw1996@naver.com>
2023-12-03 18:29:22 +0900

    fix: private route시 경고창 두 번 생성되는 버그 수정

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,15 +1,19 @@
-import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/common/useAuth';
 import { PATH } from '../constant/paths';
 
 export default function PrivateRoute({ isOnlyAdminAllowed = false, children }) {
   const { user } = useAuth();
+  const navigate = useNavigate();
 
-  if (isOnlyAdminAllowed && user?.roles[0] !== 'admin') {
-    alert('해당 페이지는 관리자만 접근이 가능합니다. 홈으로 이동합니다.');
+  useEffect(() => {
+    if (isOnlyAdminAllowed && user?.roles[0] !== 'admin') {
+      alert('해당 페이지는 관리자만 접근이 가능합니다. 홈으로 이동합니다.');
 
-    return <Navigate to={PATH.HOME} />;
-  }
+      navigate(PATH.HOME);
+    }
+  }, [isOnlyAdminAllowed, user, navigate]);
 
   return children;
 }
