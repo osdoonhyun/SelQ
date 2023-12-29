56f12e035686c171fa081d148a0b082bac36f20c
osdoonhyun <skw1996@naver.com>
2023-12-29 21:32:27 +0900

    fix: 빈 배열도 false를 return하도록 수정

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -1,13 +1,14 @@
 // 모든 key 값이 falsy인 경우 false 반환, 값이 존재하면 true
 export const hasValue = (obj) => {
-  if (obj === null) {
-    return false;
-  }
-
-  for (const key in obj) {
-    if (obj[key]) {
-      return true;
+  return Object.entries(obj).every(([_, value]) => {
+    if (
+      value === null ||
+      value === '' ||
+      value === 0 ||
+      (Array.isArray(value) && value.length === 0)
+    ) {
+      return false;
     }
-  }
-  return false;
+    return true;
+  });
 };
