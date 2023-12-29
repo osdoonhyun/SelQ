0f9a7fbac779744133975cc1fb884d00ebf27186
osdoonhyun <skw1996@naver.com>
2023-12-29 21:35:15 +0900

    fix: 임시저장 값이 null 인 경우 false return 하도록 수정

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -1,14 +1,13 @@
 // 모든 key 값이 falsy인 경우 false 반환, 값이 존재하면 true
 export const hasValue = (obj) => {
-  return Object.entries(obj).every(([_, value]) => {
-    if (
-      value === null ||
-      value === '' ||
-      value === 0 ||
-      (Array.isArray(value) && value.length === 0)
-    ) {
-      return false;
-    }
-    return true;
-  });
+  return obj === null
+    ? false
+    : Object.entries(obj).every(
+        ([_, value]) =>
+          !(
+            value === '' ||
+            value === 0 ||
+            (Array.isArray(value) && value.length === 0)
+          )
+      );
 };
