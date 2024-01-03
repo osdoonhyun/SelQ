b449ad2bf0b8b5637253d67d80f6b6edf71161a9
osdoonhyun <skw1996@naver.com>
2024-01-03 16:41:46 +0900

    refactor: 토큰 만료시 갱신 로직 slices에서 axios use로 리팩토링

--- src/store/Slices/auth.js
+++ src/store/Slices/auth.js
@@ -1,10 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getCookie, removeCookie } from '../../config/cookie';
-import { serverApi } from '../../apis/api';
+import { api, authApi } from '../../apis/api';
 
 const logIn = createAsyncThunk('user/logIn', async (userInput) => {
   try {
-    const { status } = await serverApi.post('/auth/login', userInput);
+    const { data, status } = await api.post('/auth/login', userInput);
 
     if (status === 200) {
       return getCookie('Authentication');
@@ -14,65 +14,45 @@ const logIn = createAsyncThunk('user/logIn', async (userInput) => {
   }
 });
 
-const getUserInfo = createAsyncThunk('user/userInfo', async (_, thunkAPI) => {
-  const accessToken = sessionStorage.getItem('accessToken');
+const getUserInfo = createAsyncThunk('user/userInfo', async () => {
+  try {
+    // const result = await authApi.get('/auth');
+    // console.log('RESULT', result);
+
+    const { data, status } = await authApi.get('/auth');
+    if (status === 200) {
+      return { userInfo: data?.body };
+    }
+  } catch (error) {
+    console.log('에러 발생!!!', error);
+    // if (error?.response?.statusText === 'Unauthorized') {
+    //   return getNewAccessToken(thunkAPI);
+    // } else {
+    //   throw error;
+    // }
+    throw error?.response?.data?.message;
+  }
+});
+
+const refreshAuth = async () => {
+  const refreshToken = getCookie('Refresh');
   const config = {
     headers: {
-      Authorization: 'Bearer ' + accessToken,
+      Authorization: 'Bearer ' + refreshToken,
     },
   };
 
   try {
-    const { data, status } = await serverApi.get('/auth', config);
-    if (status === 200) {
-      return { userInfo: data?.body, token: accessToken };
-    }
-  } catch (error) {
-    if (error?.response?.statusText === 'Unauthorized') {
-      return getNewAccessToken(thunkAPI);
-    } else {
-      throw error;
-    }
-  }
-});
+    const { status } = await api.get('/auth/refresh', config);
 
-const getNewAccessToken = async (thunkAPI) => {
-  const refreshToken = getCookie('Refresh');
-  try {
-    const newAccessToken = await thunkAPI.dispatch(
-      refreshAccessToken(refreshToken)
-    );
-    sessionStorage.setItem('accessToken', newAccessToken.payload);
-    thunkAPI.dispatch(getUserInfo());
-    return;
+    if (status === 200) {
+      const newAccessToken = getCookie('Authentication');
+      return newAccessToken;
+    }
   } catch (error) {
-    return thunkAPI.rejectWithValue({
-      message: 'Failed to refresh accessToken',
-    });
+    throw error;
   }
 };
-
-const refreshAccessToken = createAsyncThunk(
-  'user/refreshAccessToken',
-  async (refreshToken) => {
-    const config = {
-      headers: {
-        Authorization: 'Bearer ' + refreshToken,
-      },
-    };
-
-    try {
-      const { status } = await serverApi.get('/auth/refresh', config);
-
-      if (status === 200) {
-        const newAccessToken = getCookie('Authentication');
-        return newAccessToken;
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
-);
 
 const logOut = createAsyncThunk('user/logOut', () => {
   sessionStorage.removeItem('accessToken');
@@ -113,14 +93,6 @@ const userSlice = createSlice({
       state.user = null;
       console.log('유저정보 가져오기 실패!', action);
     });
-    builder.addCase(refreshAccessToken.fulfilled, (state, action) => {
-      console.log('토큰 재발급 성공!+++++++++++', state, action);
-      state.token = action.payload;
-    });
-    builder.addCase(refreshAccessToken.rejected, (state, action) => {
-      state.error = action.error.message;
-      console.log('토큰 재발급 실패!', action);
-    });
     builder.addCase(logOut.fulfilled, (state, action) => {
       state.isLoggedIn = false;
       state.user = null;
@@ -134,4 +106,4 @@ const userSlice = createSlice({
   },
 });
 
-export { userSlice, getUserInfo, logIn, logOut };
+export { userSlice, refreshAuth, getUserInfo, logIn, logOut };
