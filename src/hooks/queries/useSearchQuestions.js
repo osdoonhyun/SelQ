fc889f3ae2875c6673d94b3147ee56a0304c1169
osdoonhyun <87527736+osdoonhyun@users.noreply.github.com>
2024-01-05 14:32:35 +0900

    Refactor/search question (#12)
    * feat: useThrottle 쓰로틀링 훅 추가
    
    * refactor: 추천 검색어 기능 스로틀 활용하여 이벤트 및 함수 호출 최적화

--- src/hooks/queries/useSearchQuestions.js
+++ src/hooks/queries/useSearchQuestions.js
@@ -3,9 +3,14 @@ import { getQuestionsByKeyword } from '../../apis/questions';
 import { questionKeys } from '../../constant/queryKeyFactory';
 
 export const useSearchQuestions = ({ searchInput: keyword }) => {
-  const queryKey =
-    keyword.length > 0 ? questionKeys.keyword(keyword) : questionKeys.all;
-  const queryData = useQuery(queryKey, () => getQuestionsByKeyword(keyword));
+  const queryData = useQuery(
+    questionKeys.keyword(keyword),
+    () => getQuestionsByKeyword(keyword),
+    {
+      enabled: !!keyword,
+      select: (questions) => questions.slice(0, 10),
+    }
+  );
 
   return queryData;
 };
